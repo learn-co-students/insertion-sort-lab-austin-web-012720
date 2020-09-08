3d2d0170919ba320aeaa8d3aa41f5e851324b5e9
benbrower <bbrower@smu.edu>
2020-09-08 11:52:18 -0500

    find minimum and remove from array complete

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+  let index = 0;
+  let curr = array[0];
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < curr) {
+      curr = array[i];
+      index = i;
+    }
+  }
+  array.splice(index, 1);
+  return curr;
 }
 
-function selectionSort(array){
-
-}
+function selectionSort(array) {}
